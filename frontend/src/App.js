0c747cd632dcc7213b9092878c22296a647f9b51
Zach <zachalvarezxvi@gmail.com>
2022-12-09 13:34:46 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AccueilMembre from './components/AccueilMembre';
 import CreerCompte from "./components/CreerCompte";
+import Header from './components/Header';
 
 export function App(){
 
   return (
     <div>
+        <Header/>
         <Router>
           <div className='main'>
            
             <Routes>
                  <Route path='/' element={ <CreerCompte
+                                           />}>  
+     
+               </Route> 
+               <Route path='/accueil' element={ <AccueilMembre
                                            />}>  
      
                </Route> 
