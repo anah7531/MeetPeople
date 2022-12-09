f474ed1601d5c5e1128eb4160429a59c8f857a82
Zach <zachalvarezxvi@gmail.com>
2022-12-09 12:54:40 -0500

    Accueil commencer

--- frontend/src/components/AccueilMembre.jsx
+++ frontend/src/components/AccueilMembre.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Avatar } from '@mui/material';
+import avatar from '../img/avatar.jpg'
 
 const AccueilMembre = () => {
   return (
-    <div>AccueilMembre</div>
+    <Card sx={{ maxWidth: 500 }}>
+    
+     <Avatar alt="Remy Sharp" src=
+               {avatar} sx={{ width: 200, height: 200 }} />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        Virginie LaFrance
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        sexe 'F'
+        age '36'
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">J'aime pas</Button>
+      <Button size="small">J'aime</Button>
+    </CardActions>
+  </Card>
   )
 }
 
