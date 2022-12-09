2eed922af42f9852d7632954b5fdedb435a2a041
GeorgeASRA <georgeassouad90@gmail.com>
2022-12-09 08:31:36 -0500

    0.1 register frontend add

--- frontend/src/components/CreerCompte.jsx
+++ frontend/src/components/CreerCompte.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles.css";
 import logo from "../img/LogoMeetPeople.png"
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
-import axios from "axios";
+import swal from "sweetalert";
 
 //Validation des champs avec la librairie @hookform/resolvers/yup
 const schema = yup.object({
@@ -13,8 +13,8 @@ const schema = yup.object({
     prenom: yup.string().required("Le prenom est obligatoire"),
     sexe: yup.string().required(),
     dateDeNaissance: yup.string().required(),
-    interet: yup.string().required(),
-    situationFamiliale: yup.string().required(),
+    interet: yup.string().required("Choisir un intérêt"),
+    situationFamiliale: yup.string().required("Choisir une situation familiale"),
     grandeur: yup
         .number()
         .typeError("La taille est obligatoire")
@@ -22,7 +22,7 @@ const schema = yup.object({
         .min(130,"La taille min est 130 cm")
         .max(210, "La taille max est 210 cm"),
     courriel: yup.string().required("Le courriel est obligatoire").email("Courriel invalide"),
-    motDePasse: yup.string().required("Saisir un mot de passe"),
+    motDePasse: yup.string().required("Saisir un mot de passe").min(6, "Doit contenir au moins 6 caractères"),
     motDePasseConfirme: yup
         .string()
         .required("Confirmer le mot de passe")
@@ -30,19 +30,19 @@ const schema = yup.object({
 })
 
 const CreerCompte = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm(
+    const { register, handleSubmit, reset, formState: { errors } } = useForm(
         {resolver: yupResolver(schema)}
     );
 
     const postData = (data) => {
 
-        const membre ={
+        const membre = {
             courriel: data.courriel,
             nom: data.nom,
             prenom: data.prenom,
             motDePasse: data.motDePasse,
             estActif: 1,
-            detailProfil: "Ecrire une description de toi",
+            detailProfil: "Allô, je suis nouveau ici",
             sexe: data.sexe,
             grandeur: data.grandeur,
             dateDeNaissance: data.dateDeNaissance,
@@ -51,14 +51,25 @@ const CreerCompte = () => {
             idInteret: data.interet,
             numeroAdminDesactive: null
         }
+        
+        //Envoie les données à l'API
+        fetch("http://localhost:8080/auth/register", {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(membre)
+        })
 
-       axios.post("http://localhost:8080/auth/register", membre)
-        .then(response => {
-        if(response.data != null){
-          console.log(membre);
-          alert("Vous êtes enregistré")
-        }
-      })
+        //Reset le formulaire
+        reset();
+
+        //Alert de confirmation
+        swal({
+            title: "Vous êtes enregistré",
+            icon: "success",
+            button: "Accepter"
+        })
     }
 
     return (
@@ -86,7 +97,7 @@ const CreerCompte = () => {
                         <div className="col">
                             <label>Prenom</label><br/>
                             <input type="text" className="form-control" {...register('prenom')} placeholder="Prenom"/>
-                            <small className="text-danger">{errors.prenom?.message}</small>
+                            <small className="text-danger">{errors.prenom?.message}</small><br/>
                         </div>
                     </div>
                     <div className="row">
@@ -106,26 +117,28 @@ const CreerCompte = () => {
                         <div className="col">
                             <label>Intérêt</label>
                             <select {...register('interet')} className="form-control">
-                                <option selected="true" disabled="disabled" value="">Choisir un intérêt</option>
+                                <option selected={true} disabled={"disabled"} value="">Choisir un intérêt</option>
                                 <option value="1">Relation serieuse</option>
                                 <option value="2">Amitié</option>
                                 <option value="3">Relation non serieuse</option>
                                 <option value="4">Marriage</option>
                             </select>
-                            {errors.interet && <small className="text-danger">Choisir un intérêt</small>}
+                            <small className="text-danger">{errors.interet?.message}</small>
+                            {/* {errors.interet && <small className="text-danger">Choisir un intérêt</small>} */}
                         </div>
                         <div className="col">
                             <label>Situation Familiale</label>
-                            <select {...register('situationFamiliale', { required:true })} className="form-control">
-                                <option selected="true" disabled="disabled" value="">Choisir une situation familiale</option>
+                            <select {...register('situationFamiliale')} className="form-control">
+                                <option selected={true} disabled={"disabled"} value={""} >Choisir une situation familiale</option>
                                 <option value="1">A des enfants</option>
                                 <option value="2">N'a pas d'enfants</option>
                             </select>
-                            {errors.situationFamiliale && <small className="text-danger">Choisir une situation familiale</small>}<br/>
+                            <small className="text-danger">{errors.situationFamiliale?.message}</small>
+                            {/* {errors.situationFamiliale && <small className="text-danger">Choisir une situation familiale</small>} */}<br/>
                         </div>
                     </div>
                     <label>Taille (en cm)</label>
-                    <input type="number" className="form-control" {...register('grandeur', {required:true})} placeholder="000"/>
+                    <input type="number" className="form-control" {...register('grandeur')} placeholder="000"/>
                     <small className="text-danger">{errors.grandeur?.message}</small>
                     <br/>
                     <label>Courriel</label>
@@ -140,12 +153,12 @@ const CreerCompte = () => {
                         </div>
                         <div className="col">
                             <label>Confirmer le mot de passe</label>
-                            <input type="password" className="form-control" {...register('motDePasseConfirme')} errorMe/>
+                            <input type="password" className="form-control" {...register('motDePasseConfirme')}/>
                             <small className="text-danger">{errors.motDePasseConfirme?.message}</small><br/>
                         </div>
                     </div>
                 <div className="text-center mt-2">
-                <input type="submit" className="btn colorBtn rounded-5 text-white" value="Créer compte"/>
+                <input type="submit" className="btn colorBtn rounded-5 text-white" value={"Créer compte"}/>
                 </div>
                 </form>
             </div>
