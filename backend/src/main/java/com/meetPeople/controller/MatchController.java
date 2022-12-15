f8c20a34a169706c1995dcfd50ffbfe54bb7cab5
Ana Hernandez-Smith <ana.hernandez7531@gmail.com>
2022-12-14 19:52:06 -0500

    Aimer ou pas aimer done.

--- backend/src/main/java/com/meetPeople/controller/MatchController.java
+++ backend/src/main/java/com/meetPeople/controller/MatchController.java
@@ -1,11 +1,15 @@
 package com.meetPeople.controller;
 
 
+import com.meetPeople.entity.MatchTable;
 import com.meetPeople.model.Profile;
+import com.meetPeople.repository.MatchTableRepository;
 import com.meetPeople.services.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -20,4 +24,11 @@ public class MatchController {
     public List<Profile> getMyMatches(@PathVariable int id){
         return matchService.getMyMatches(id);
     }
+
+
+    @PostMapping("/onClickForLikeOrNot/{idMembreInitiateur}/{idMembreSecond}/{estAimerParInitiateur}")
+    public Integer onClickForLikeOrNot(@PathVariable int idMembreInitiateur,@PathVariable int idMembreSecond, @PathVariable Boolean estAimerParInitiateur){
+        return matchService.onClickForLikeOrNot(idMembreInitiateur,idMembreSecond, estAimerParInitiateur);
+    }
+
 }
--- backend/src/main/java/com/meetPeople/services/MatchService.java
+++ backend/src/main/java/com/meetPeople/services/MatchService.java
@@ -1,14 +1,17 @@
 package com.meetPeople.services;
 
+import com.meetPeople.controller.MatchController;
 import com.meetPeople.entity.MatchTable;
 import com.meetPeople.entity.MatchTablePk;
 import com.meetPeople.model.Profile;
 import com.meetPeople.repository.MatchTableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 @Service
 public class MatchService {
@@ -17,6 +20,7 @@ public class MatchService {
     MatchTableRepository matchTableRepository;
     @Autowired
     ProfileService profileService;
+
 
     public List<Profile> getMyMatches(int id){
         List<Integer> matchesId = matchTableRepository.getMyMatchesId(id);
@@ -32,9 +36,35 @@ public class MatchService {
         return profileList;
     }
 
-    public boolean isMatch(int myId, int otherUserId){
+    public Boolean isMatch(int myId, int otherUserId){
         MatchTable match1 = matchTableRepository.findById(new MatchTablePk(myId, otherUserId)).orElse(null);
 
-        return match1 != null && match1.isEstAimerParInitiateur() && match1.getEstAimerParSecond();
+        return match1 != null && match1.getEstAimerParInitiateur() && match1.getEstAimerParSecond();
     }
+
+    //returns 2 if the records are inserted and returns 1 if there was an update
+    public  Integer onClickForLikeOrNot(int idMembreInitiateur, int idMembreSecond, Boolean estAimerParInitiateur){
+
+        if(matchTableRepository.srchPrevLikeBetween2Mems(idMembreInitiateur, idMembreSecond).isEmpty()) {
+
+            //list empty -> no record -> Insert 2 rows
+            return matchTableRepository.insertLike(idMembreInitiateur, idMembreSecond, estAimerParInitiateur);
+
+        } else {
+            //list have record -> records found -> Update 2 rows
+
+            if(matchTableRepository.updateMatchInitiateur(idMembreInitiateur, idMembreSecond, estAimerParInitiateur) == 1 &&
+                    matchTableRepository.updateMatchSecond(idMembreInitiateur, idMembreSecond, estAimerParInitiateur) == 1  )
+                return 1;
+
+            else return 0;
+
+        }
+
+    }
+
+
+
+
+
 }
